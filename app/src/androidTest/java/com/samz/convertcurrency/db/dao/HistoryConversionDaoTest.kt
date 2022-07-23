2f814820b659860e21f5a45b5cec504fb5a434bd
samz <bassamsaber46@gmail.com>
2022-07-24 01:05:31 +0200

    - hilt with database testing

--- app/src/androidTest/java/com/samz/convertcurrency/db/dao/HistoryConversionDaoTest.kt
+++ app/src/androidTest/java/com/samz/convertcurrency/db/dao/HistoryConversionDaoTest.kt
@@ -1,42 +1,41 @@
 package com.samz.convertcurrency.db.dao
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import com.samz.convertcurrency.db.AppDatabase
 import com.samz.convertcurrency.model.ConvertedCurrencies
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import java.text.SimpleDateFormat
 import java.util.*
+import javax.inject.Inject
+import javax.inject.Named
 
-@OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
 @SmallTest
+@HiltAndroidTest
 class HistoryConversionDaoTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database: AppDatabase
+    @Inject
+    @Named("test_db")
+    lateinit var database: AppDatabase
     private lateinit var dao: HistoryConversionDao
 
     @Before
     fun setup() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            AppDatabase::class.java
-        ).allowMainThreadQueries()
-            .build()
-
+        hiltRule.inject()
         dao = database.conversionDao()
     }
 
---
+++ app/src/androidTest/java/com/samz/convertcurrency/di/TestAppModule.kt
@@ -0,0 +1,26 @@
+package com.samz.convertcurrency.di
+
+import android.content.Context
+import androidx.room.Room
+import com.samz.convertcurrency.db.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Named("test_db")
+    fun provideInMemoryDb(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(
+            context,
+            AppDatabase::class.java
+        ).allowMainThreadQueries()
+            .build()
+
+}
